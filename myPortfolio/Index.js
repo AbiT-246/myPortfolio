98352d952860ae69c81b24488a5c7c75b8e107dd
Abigia Debebe <Abitibebu123@gmail.com>
2023-06-30 14:19:31 +0300

    first page pics without blurbs

--- myPortfolio/Index.js
+++ myPortfolio/Index.js
@@ -9,8 +9,20 @@ let spotify = document.getElementById("spotify");
 
 let images1 = document.getElementById("images1");
 let images2 = document.getElementById("images2");
-let specTitle = document.getElementById("itself");
-let specTitle2 = document.getElementById("itself2");
+let itself = document.getElementById("itself");
+let itself2 = document.getElementById("itself2");
+let images11 = document.getElementById("images11");
+let images22 = document.getElementById("images22");
+let images111 = document.getElementById("images111");
+let images222 = document.getElementById("images222");
+let images1111 = document.getElementById("images1111");
+let images2222 = document.getElementById("images2222");
+let itself11 = document.getElementById("itself11");
+let itself22 = document.getElementById("itself22");
+let itself111 = document.getElementById("itself111");
+let itself222 = document.getElementById("itself222");
+let itself1111 = document.getElementById("itself1111");
+let itself2222 = document.getElementById("itself2222");
 
 window.addEventListener("scroll", () => {
   let yAxis = window.scrollY;
@@ -55,8 +67,34 @@ window.addEventListener("scroll", () => {
     let images2Offset = yAxis - 700;
     images2.style.transform = `translateX(${-images2Offset * 0.5}px)`;
     let titleOffset = yAxis - 1000;
-    specTitle.style.transform = `translateY(${-titleOffset * 0.7}px)`;
-    specTitle2.style.transform = `translateY(${-titleOffset * 0.7}px)`;
+    itself.style.transform = `translateY(${-titleOffset * 0.5}px)`;
+    itself2.style.transform = `translateY(${-titleOffset * 0.5}px)`;
+    console.log(titleOffset * 0.5);
+  }
+  if (yAxis >= 1700 && yAxis <= 2400) {
+    let images11Offset = yAxis - 1700;
+    images11.style.transform = `translateX(${images11Offset * 0.5}px)`;
+    let images22Offset = yAxis - 1700;
+    images22.style.transform = `translateX(${-images22Offset * 0.5}px)`;
+    let titleOffset = yAxis - 2000;
+    itself11.style.transform = `translateY(${-titleOffset * 0.5}px)`;
+    itself22.style.transform = `translateY(${-titleOffset * 0.5}px)`;
+    console.log(titleOffset * 0.5);
+  }
+  if (yAxis >= 3000 && yAxis <= 3700) {
+    let images111Offset = yAxis - 3000;
+    images111.style.transform = `translateX(${images111Offset * 0.5}px)`;
+    let images222Offset = yAxis - 3000;
+    images222.style.transform = `translateX(${-images222Offset * 0.5}px)`;
+    let titleOffset = yAxis - 3300;
+    itself111.style.transform = `translateY(${-titleOffset * 0.7}px)`;
+    itself222.style.transform = `translateY(${-titleOffset * 0.7}px)`;
+    console.log(titleOffset * 0.5);
+  }
+  if (yAxis >= 4000 && yAxis <= 4700) {
+    let titleOffset = yAxis - 4300;
+    itself1111.style.transform = `translateY(${-titleOffset * 0.7}px)`;
+    itself2222.style.transform = `translateY(${-titleOffset * 0.7}px)`;
     console.log(titleOffset * 0.5);
   }
 });
--- myPortfolio/Projects.js
+++ myPortfolio/Projects.js
@@ -1,6 +1,13 @@
 let body = document.getElementsByTagName("body")[0];
-let projectTitle = document.getElementById("projectTitle");
-let projectDesc = document.getElementById("projectDesc");
+let projectTitles = document.getElementsByClassName("projectTitle");
+let projectDescs = document.getElementsByClassName("projectDesc");
+let projectTitle = projectTitles[0];
+let projectDesc = projectDescs[0];
+let projectTitle2 = projectTitles[1];
+console.log(projectTitle2);
+let projectDesc2 = projectDescs[1];
+let projectTitle3 = projectTitles[2];
+let projectDesc3 = projectDescs[2];
 
 let image = document.getElementById("projImage");
 
@@ -16,4 +23,20 @@ window.addEventListener("scroll", () => {
     projectDesc.style.transform = `translateY(${-titleOffset * 0.7}px)`;
     console.log(titleOffset * 0.5);
   }
+  if (yAxis >= 900 && yAxis <= 1050) {
+    // let imageOffset = yAxis;
+    // image.style.transform = `translateX(${-imageOffset * 0.7}px)`;
+    let titleOffset = yAxis - 600;
+    projectTitle2.style.transform = `translateY(${-titleOffset * 0.7}px)`;
+    projectDesc2.style.transform = `translateY(${-titleOffset * 0.7}px)`;
+    // console.log(titleOffset * 0.5);
+  }
+  // if (yAxis >= 175 && yAxis <= 417) {
+  //   let imageOffset = yAxis;
+  //   image.style.transform = `translateX(${-imageOffset * 0.7}px)`;
+  //   let titleOffset = yAxis;
+  //   projectTitle.style.transform = `translateY(${-titleOffset * 0.7}px)`;
+  //   projectDesc.style.transform = `translateY(${-titleOffset * 0.7}px)`;
+  //   console.log(titleOffset * 0.5);
+  // }
 });
